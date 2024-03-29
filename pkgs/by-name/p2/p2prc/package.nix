289e1226bd876803a8274a1491ed004b7ed085c9
Akilan <me@akilan.io>
2024-03-29 13:14:16 +0000

    saving current changs

--- pkgs/by-name/p2/p2prc/package.nix
+++ pkgs/by-name/p2/p2prc/package.nix
@@ -11,12 +11,14 @@ buildGoModule rec {
     src = fetchFromGitHub {
         owner   = "akilan1999";
         repo    = "p2p-rendering-computation";
-        rev     = "v${version}";
-        hash    = "sha256-LW6UKEF9txWXyVP2deRiWFuNqfNTp0ZfXZs+n+TbAZc=";
+        rev     = "sha256-aa4b18f39d260c94d84ff2957e4e24c6813ee38a";
+        # hash    = "sha256-LW6UKEF9txWXyVP2deRiWFuNqfNTp0ZfXZs+n+TbAZc=";
     };
 
-    deleteVendor = true;
+    #deleteVendor = true;
     #proxyVendor = true;
+
+    vendorSha256 = lib.fakeSha256;
 
     #outputs = [ "docker" ];
 
